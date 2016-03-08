1bbc7e42df5100b4d293127ad0f4d993fb9c4c27
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-03-07 20:55:17 -0500

    Refactored drawBoard() function

--- script.js
+++ script.js
@@ -2,12 +2,11 @@
  *	Check PhoneGap to port onto iOS/Android
  */
 
-var width = 500;
-var height = 500;
 var boxsize = 50; //Circle diameter
 var indist = 3;	  // distance between circles
-var circleRadius;
-var canvasBuffer = 10;
+var buff = 10; //canvasBuffer
+var width = (boxsize + indist) * 7 + 2 * buff; //7 circles horizontal
+var height = (boxsize + indist) * 6 + 2 * buff; //6 circles vertical
 var context;
 var drawBoardBool;
 
@@ -29,19 +28,18 @@ function drawBoard() {
 	//Clear canvas before starting
 	context.clearRect(0, 0, width, height);
 
-	circleRadius = ((height - 2*canvasBuffer) - 6*indist) / 12.0;
+	size = canvas.height - 2*buff; 		// actual size without the surrounding buffer
+    radius =  (size - 6*indist)/12.0; 	// radius of each colored disk
 
 	//Drawing the circles on the canvas
 	for (var i = 0; i < 7; i++) {
 		for (var j = 0; j < 6; j++) {
 
 			context.beginPath(); //Set up canvas
-		    context.arc(i*(boxsize + indist) + canvasBuffer + circleRadius, j*(boxsize+indist) + canvasBuffer + circleRadius, circleRadius, 0, 2*Math.PI);		// draw an arc at (x,y) of radius r, from 0 to 2 pi angles
-		    if (drawBoardBool) {					// if true then set fillStyle to given color and apply fill
-		        context.fillStyle = "#000000"; //Quotes
-		        		        context.fill();						
-		    }
-		    context.strokeStyle = "#000000";			// color of the circle
+
+			context.arc(i*(boxsize+indist)+buff+radius, j*(boxsize+indist)+buff+radius, radius, 0, 2*Math.PI);		// draw an arc at (x,y) of radius r, from 0 to 2 pi angles
+
+		    context.strokeStyle = "#000000"; //Set circle color
 		    context.stroke();	
 
 		}
